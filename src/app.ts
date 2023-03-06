c0aed3d9585709e3c4b635b603cf4ab19ea52397
Evomatic <evantrujillo30@gmail.com>
2023-03-06 20:52:12 +0900

    Format app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 function render() {
-    const templateElement = document.getElementById('project-input') as HTMLTemplateElement
-    const hostElement = document.getElementById('app') as HTMLDivElement
-    const importNode = document.importNode(templateElement.content, true)
-    const element = importNode.firstElementChild as HTMLFormElement
-    hostElement.insertAdjacentElement('afterbegin', element)
+  const templateElement = document.getElementById(
+    'project-input'
+  ) as HTMLTemplateElement;
+  const hostElement = document.getElementById('app') as HTMLDivElement;
+  const importNode = document.importNode(templateElement.content, true);
+  const element = importNode.firstElementChild as HTMLFormElement;
+  hostElement.insertAdjacentElement('afterbegin', element);
 }
 
-render()
+render();
