8ca6912cc4e50ef9ca5a0a9450affb13a466b8dc
evomatic <evantrujillo30@gmail.com>
2023-04-13 14:25:53 +0900

    WIP: refactor to create list item with content

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 type ProjectInputData = Record<string, unknown | string> & {
   title: string;
   description: string;
@@ -15,6 +16,14 @@ class ProjectBase {
       description: '',
       people: 0
     };
+  }
+
+  createNewElement(element: string) {
+    return document.createElement(element) as HTMLElement;
+  }
+
+  createNewTextNode(value: string) {
+    return document.createTextNode(value);
   }
 
   render(getElementById: string, elementPosition: InsertPosition): void {
@@ -36,9 +45,17 @@ class ProjectInput extends ProjectBase {
 
   addProject(): void {
     const formValues = Object.values(this.formData);
-    [...formValues].forEach((value) => {
-      const newListItem = document.createElement('li') as HTMLElement;
-      const listItemValue = document.createTextNode(value as string);
+    [...formValues].forEach((value, index) => {
+      const newListItem = this.createNewElement('li');
+      const newH2Element = this.createNewElement('h2');
+      const newH3Element = this.createNewElement('h3');
+      const newPElement = this.createNewElement('p');
+
+      const listItemValue = this.createNewTextNode(value as string);
+      const newH2Value = this.createNewTextNode(value as string);
+      const newH3Value = this.createNewTextNode(value as string);
+      const newPValue = this.createNewTextNode(value as string);
+
       newListItem.appendChild(listItemValue);
       const getActiveProjectById: HTMLElement = document.getElementById(
         'active projects'
