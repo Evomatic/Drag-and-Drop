3ea5f63df55dc84681cf46b41a175279ca1d6d8f
Evomatic <evantrujillo30@gmail.com>
2023-05-08 19:36:37 +0900

    Add input validation and clear input after submit

--- src/app.ts
+++ src/app.ts
@@ -103,33 +103,58 @@ class ProjectInput extends ProjectBase {
 
   addProject(): void {
     const newListItem = this.createNewElement('li');
-    const { title, description, people } = this.formData;
+    if (!this.isProjectInputValid(this.formData)) {
+      return;
+    } else {
+      const { title, description, people } = this.formData;
 
-    const newH2Element = this.createNewElement('h2');
-    const newH3Element = this.createNewElement('h3');
-    const newPElement = this.createNewElement('p');
+      const newH2Element = this.createNewElement('h2');
+      const newH3Element = this.createNewElement('h3');
+      const newPElement = this.createNewElement('p');
 
-    const newH2Value = this.createNewTextNode(title);
-    const newH3Value = this.createNewTextNode(`${people} persons assigned`);
-    const newPValue = this.createNewTextNode(description);
+      const newH2Value = this.createNewTextNode(title);
+      const newH3Value = this.createNewTextNode(`${people} persons assigned`);
+      const newPValue = this.createNewTextNode(description);
 
-    newH2Element.appendChild(newH2Value);
-    newH3Element.appendChild(newH3Value);
-    newPElement.appendChild(newPValue);
+      newH2Element.appendChild(newH2Value);
+      newH3Element.appendChild(newH3Value);
+      newPElement.appendChild(newPValue);
 
-    newListItem.appendChild(newH2Element);
-    newListItem.appendChild(newH3Element);
-    newListItem.appendChild(newPElement);
-    newListItem.setAttribute('draggable', 'true');
-    newListItem.setAttribute('id', this.generateRandomId());
-    newListItem.setAttribute('class', 'draggable');
-    newListItem.addEventListener('dragstart', this.handleDragStart);
-    newListItem.addEventListener('dragend', this.handleDragEnd);
+      newListItem.appendChild(newH2Element);
+      newListItem.appendChild(newH3Element);
+      newListItem.appendChild(newPElement);
+      newListItem.setAttribute('draggable', 'true');
+      newListItem.setAttribute('id', this.generateRandomId());
+      newListItem.setAttribute('class', 'draggable');
+      newListItem.addEventListener('dragstart', this.handleDragStart);
+      newListItem.addEventListener('dragend', this.handleDragEnd);
 
-    const getActiveProjectById: HTMLElement = document.getElementById(
-      'active projects'
-    ) as HTMLElement;
-    getActiveProjectById.appendChild(newListItem);
+      const getActiveProjectById: HTMLElement = document.getElementById(
+        'active projects'
+      ) as HTMLElement;
+      getActiveProjectById.appendChild(newListItem);
+    }
+    this.clearProjectInput();
+  }
+
+  isProjectInputValid(formData: ProjectInputData): boolean {
+    const { title, description, people } = formData;
+
+    if (
+      title.trim().length === 0 ||
+      description.trim().length === 0 ||
+      Number(people) === 0 ||
+      Number(people) > 5
+    ) {
+      alert('All fields must be valid');
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  clearProjectInput(): void {
+    (document.getElementById('project-form') as HTMLFormElement).reset();
   }
 
   getFormData(): void {
